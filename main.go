c9c6319858a2a21a95a3a2c2eb38435eff87979c
vatsal278 <vatsalchaturvedi278@gmail.com>
2022-04-12 16:52:45 +0530

    commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"user_auth/controller"
+	"user_auth/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	var loginService service.LoginService = service.StaticLoginService()
+	var jwtService service.JWTService = Service.JWTAuthService()
+	var loginController controller.LoginController = controller.LoginHandler(loginService, jwtService)
+
+	server := gin.New()
+	server.POST("/login", func(ctx *gin.Context) {
+		token := loginController.Login(ctx)
+		if token != "" {
+			ctx.JSON(http.StatusOK, gin.H{
+				"token": token,
+			})
+		} else {
+			ctx.JSON(http.StatusUnauthorized, nil)
+		}
+	})
+
+	server.Run(":8080")
+}
