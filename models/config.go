d95a35b5f50685b42911971ea93d9e1861b5c618
vatsal278 <vatsalchaturvedi278@gmail.com>
2022-05-12 19:34:16 +0530

    passing db as environment variable

--- models/config.go
+++ models/config.go
@@ -1,14 +1,14 @@
 package models
 
 import (
+	"os"
 	"user_auth/db"
 )
 
-var server = "172.18.0.2:27017"
+var server = os.Getenv("DBADDRESS")
 
-// var server = os.Getenv("DATABASE")
 // Database name
-var databaseName = "users"
+var databaseName = os.Getenv("DBNAME")
 
 // Create a connection
 var dbConnect = db.NewConnection(server, databaseName)
